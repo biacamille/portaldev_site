4c36a0b73ed65b79e6397089fb42cc68d0997b0b
Beatriz Camille <biacamille2019@gmail.com>
2024-03-28 21:01:26 +0000

    Modifica frase da página (Em Construção)

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-  return <h1>Portal Dev</h1>;
+  return (
+    <h1>
+      Portal Dev:Este site é dedicado à criação de conteúdos relacionados à
+      tecnologia, visando auxiliar iniciantes a estabelecerem uma sólida base na
+      área da programação.
+    </h1>
+  );
 }
 
 export default Home;
